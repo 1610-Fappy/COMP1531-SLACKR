f9dbe9714f57480662d5774b95090f49f296db52
Abdul Kanj <a.kanj@student.unsw.edu.au>
2020-03-05 17:31:29 +1100

    removed line checking tokens - not needed

--- tests/authLogin_test.py
+++ tests/authLogin_test.py
@@ -12,37 +12,28 @@ ASSUMPTIONS
 def test_login():
     user1Register = auth_register('HotGuy420@gmail.com', 'verySecureP@55word', 'Sylvester', 'Stallone') 
     user1Register_id = user1Register['u_id']
-    user1Register_token = user1Register['token']
 
     user1Login = auth_login('HotGuy420@gmail.com', 'verySecureP@55word')
     user1Login_id = user1Login['u_id']
-    user1Login_token = user1Login['token']
 
     assert user1Login_id == user1Register_id
-    #Not sure if this line will work
-    assert user1Login_token == user1Register_token
+
     
     user2Register = auth_register('C00LGUY@hotmail.com', 'aVeryC00lguy', 'Bruce', 'Willis')
     user2Register_id = user2Register['u_id']
-    user2Register_token = user2Register['token']
 
     user2Login = auth_login('c00LGUY@hotmail.com', 'aVeryC00lguy')
     user2Login_id = user2Login['u_id']
-    user2Login_token = user2Login['token']
 
     assert user2Login_id == user2Register_id
-    assert user2Login_token == user2Register_token
 
     user3Register = auth_register('z9398627@unsw.edu.au', 'Shr3k15lyfe', 'Mickey', 'Mouse')
     user3Register_id = user3Register['u_id']
-    user3Register_token = user3Register['token']
 
     user3Login = auth_login('z9398627@unsw.edu.au', 'Shr3k15lyfe')
     user3Login_id = user3Login['u_id']
-    user3Login_token = user3Login['token']
 
     assert user3Login_id == user3Register_id
-    assert user3Login_token == user3Register_token
 
 def test_loginPasswordIncorrect():
 
