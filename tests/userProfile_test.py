56928caa713c8b7608c3c2edd1a115755cc57b51
Abdul Kanj <a.kanj@student.unsw.edu.au>
2020-03-07 14:07:25 +1100

    Began Working on testing user_profile function, added a user

--- tests/userProfile_test.py
+++ tests/userProfile_test.py
@@ -1,1 +1,12 @@
 from error import InputError
+from src.user import user_profile
+from src.auth import auth_register, auth_login
+import pytest
+
+def test_userProfile():
+
+    auth_register('knight360@hotmail.com', 'KillerX123', 'Michael', 'Vo')
+    user1Login = auth_login('knight360@hotmail.com', 'KillerX123')
+    user1Profile = user_profile(user1Login['token'], user1Login['u_id'])
+
+    
