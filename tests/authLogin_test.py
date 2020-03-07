20c2d4d61fbf164fb79640ea0e7a32645fd69d16
Abdul Kanj <a.kanj@student.unsw.edu.au>
2020-03-07 16:42:46 +1100

    Fixed not logging out before logging back in

--- tests/authLogin_test.py
+++ tests/authLogin_test.py
@@ -1,4 +1,4 @@
-from src.auth import auth_register, auth_login
+from src.auth import auth_register, auth_login, auth_logout
 import pytest
 from error import InputError
 
@@ -12,6 +12,7 @@ ASSUMPTIONS
 def test_login():
     user1Register = auth_register('HotGuy420@gmail.com', 'verySecureP@55word', 'Sylvester', 'Stallone') 
     user1Register_id = user1Register['u_id']
+    auth_logout[user1Register['token']]
 
     user1Login = auth_login('HotGuy420@gmail.com', 'verySecureP@55word')
     user1Login_id = user1Login['u_id']
@@ -21,6 +22,7 @@ def test_login():
     
     user2Register = auth_register('C00LGUY@hotmail.com', 'aVeryC00lguy', 'Bruce', 'Willis')
     user2Register_id = user2Register['u_id']
+    auth_logout[user2Register['token']]
 
     user2Login = auth_login('c00LGUY@hotmail.com', 'aVeryC00lguy')
     user2Login_id = user2Login['u_id']
@@ -29,6 +31,7 @@ def test_login():
 
     user3Register = auth_register('z9398627@unsw.edu.au', 'Shr3k15lyfe', 'Mickey', 'Mouse')
     user3Register_id = user3Register['u_id']
+    auth_logout[user3Register['token']]
 
     user3Login = auth_login('z9398627@unsw.edu.au', 'Shr3k15lyfe')
     user3Login_id = user3Login['u_id']
@@ -40,6 +43,9 @@ def test_loginPasswordIncorrect():
     user1 = auth_register('HotGuy420@gmail.com', 'verySecureP@55word', 'Sylvester', 'Stallone') 
     user2 = auth_register('C00LGUY@hotmail.com', 'aVeryC00lguy', 'Bruce', 'Willis')
     user3 = auth_register('z9398627@unsw.edu.au', 'Shr3k15lyfe', 'Mickey', 'Mouse')
+    auth_logout[user1['token']]
+    auth_logout[user2['token']]
+    auth_logout[user3['token']]
 
     with pytest.raises(InputError) as e:
         auth_login('HotGuy420@gmail.com', 'wrongpass')
