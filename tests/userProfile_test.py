1fb38b77da56387b8fd0af18dc6938323b992ce6
Abdul Kanj <a.kanj@student.unsw.edu.au>
2020-03-07 16:59:23 +1100

    Added loggging out after registering

--- tests/userProfile_test.py
+++ tests/userProfile_test.py
@@ -1,35 +1,41 @@
 from error import InputError
 from src.user import user_profile
-from src.auth import auth_register, auth_login
+from src.auth import auth_register, auth_login, auth_logout
 import pytest
 
 def test_userProfile():
 
-    auth_register('knight360@hotmail.com', 'KillerX123', 'Michael', 'Vo')
+    user1 = auth_register('knight360@hotmail.com', 'KillerX123', 'Michael', 'Vo')
+    auth_logout(user1['token'])
     user1Login = auth_login('knight360@hotmail.com', 'KillerX123')
     user1Profile = user_profile(user1Login['token'], user1Login['u_id'])
 
-    auth_register('killerabz@gmail.com', 'P@55word', 'Abdul', 'Kanj')
+    user2 = auth_register('killerabz@gmail.com', 'P@55word', 'Abdul', 'Kanj')
+    auth_logout(user2['token'])
     user2Login = auth_login('killerabz@gmail.com', 'P@55word')
     user2Profile = user_profile(user2Login['token'], user2Login['u_id'])
 
-    auth_register('mrman@cse.unsw.edu.au', 'hello21234', 'Big', 'Man')
+    user3 = auth_register('mrman@cse.unsw.edu.au', 'hello21234', 'Big', 'Man')
+    auth_logout(user3['token'])
     user3Login = auth_login('mrman@cse.unsw.edu.au', 'hello21234')
     user3Profile = user_profile(user3Login['token'], user3Login['u_id'])
 
 def test_invalidID():
 
-    auth_register('knight360@hotmail.com', 'KillerX123', 'Michael', 'Vo')
+    user1 = auth_register('knight360@hotmail.com', 'KillerX123', 'Michael', 'Vo')
+    auth_logout(user1['token'])
     user1Login = auth_login('knight360@hotmail.com', 'KillerX123')
     with pytest.raises(InputError) as e:
         user1Profile = user_profile(user1Login['token'], 'InvalidID')
 
-    auth_register('killerabz@gmail.com', 'P@55word', 'Abdul', 'Kanj')
+    user2 = auth_register('killerabz@gmail.com', 'P@55word', 'Abdul', 'Kanj')
+    auth_logout(user2['token'])
     user2Login = auth_login('killerabz@gmail.com', 'P@55word')
     with pytest.raises(InputError) as e:
         user2Profile = user_profile(user2Login['token'], 'InvalidID')
 
-    auth_register('mrman@cse.unsw.edu.au', 'hello21234', 'Big', 'Man')
+    user3 = auth_register('mrman@cse.unsw.edu.au', 'hello21234', 'Big', 'Man')
+    auth_logout(user3['token'])
     user3Login = auth_login('mrman@cse.unsw.edu.au', 'hello21234')
     with pytest.raises(InputError) as e:
         user3Profile = user_profile(user3Login['token'], 'InvalidID')
