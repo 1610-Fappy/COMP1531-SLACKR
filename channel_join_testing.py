dd357b5bff6641c6685ee32bf93835b209cae565
Jonathan <z5255742@unsw.edu.au>
2020-03-07 17:16:32 +1100

    channel_join_testing.py

---
+++ channel_join_testing.py
@@ -0,0 +1,26 @@
+from src.channel import channel_join
+from src.auth import auth_register, auth_login
+from src.channels import channels_create
+import pytest
+from src.error import InputError, AccessError
+
+def testing_channel_join():
+    User1 = auth_register('C00lguy@gmail.com', 'SickM0de', 'Sicko', 'Mode')
+    User2 = auth_register('UnC00lguy@gmail.com', 'NotSick0Mode', 'NotSicko', 'Mode')
+    channel1 = channels_create(User1['Token'], 'FirstChannel', True)
+    PrivateChannel = channels_create(User1['Token'], 'MyPrivateChannel', False)
+    channel_join(User2['Token'], channel1['channel_id'])
+
+    with pytest.raises(InputError) as e:
+        channel_join(User2['Token'], '-1')
+    with pytest.raises(AccessError) as e:
+        channel_join(User2['Token'], PrivateChannel['channel_id'])
+
+    CoolUser = auth_register('CoolUser@gmail.com', 'Pa55W0rd', 'Cool', 'User')
+    NotSoCool = auth_register('WishIWasCool@gmail.com', 'BadPass1ame', 'NotSo', 'Cool')
+    NewChannel = channels_create(CoolUser['Token'], 'PrivateChannel', False)
+
+    with pytest.raises(InputError) as e:
+        channel_join(NotSoCool['Token'], '-1')
+    with pytest.raises(AccessError) as e:
+        channel_join(NotSoCool['Token'], NewChannel['channel_id'])
