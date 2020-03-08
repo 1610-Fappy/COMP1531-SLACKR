d552e8180a9097e8f9d1237207069945f48c9e89
Abdul Kanj <a.kanj@student.unsw.edu.au>
2020-03-08 15:48:07 +1100

    Added Access Error Tests

--- tests/userProfile_test.py
+++ tests/userProfile_test.py
@@ -1,4 +1,4 @@
-from src.error import InputError
+from src.error import InputError, AccessError
 from src.user import user_profile
 from src.auth import auth_register, auth_login, auth_logout
 import pytest
@@ -39,3 +39,21 @@ def test_invalidID():
     user3Login = auth_login('mrman@cse.unsw.edu.au', 'hello21234')
     with pytest.raises(InputError) as e:
         user3Profile = user_profile(user3Login['token'], 'InvalidID')
+
+def test_validToken():
+
+    user1 = auth_register('knight360@hotmail.com', 'KillerX123', 'Michael', 'Vo')
+    auth_logout(user1['token'])
+    #Token is invalid after logging out
+    with pytest.raises(InputError) as e:
+        user1Profile = user_profile(user1Login['token'], user1['u_id'])
+
+    user2 = auth_register('killerabz@gmail.com', 'P@55word', 'Abdul', 'Kanj')
+    auth_logout(user2['token'])
+    with pytest.raises(InputError) as e:
+        user2Profile = user_profile(user2Login['token'], user2['u_id'])
+
+    user3 = auth_register('mrman@cse.unsw.edu.au', 'hello21234', 'Big', 'Man')
+    auth_logout(user3['token'])
+    with pytest.raises(InputError) as e:
+        user3Profile = user_profile(user3Login['token'], user3['u_id'])
