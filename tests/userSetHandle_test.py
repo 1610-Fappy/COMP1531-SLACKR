55c4c80925256254f0597e7f68ee2c8a8bb616c6
Abdul Kanj <a.kanj@student.unsw.edu.au>
2020-03-08 16:00:34 +1100

    Added AccessError Tests

--- tests/userSetHandle_test.py
+++ tests/userSetHandle_test.py
@@ -1,6 +1,6 @@
-from src.error import InputError
+from src.error import InputError, AccessError
 from src.user import user_profile_sethandle
-from src.auth import auth_register
+from src.auth import auth_register, auth_logout
 
 
 def test_setHandle():
@@ -31,3 +31,20 @@ def test_handleUsed():
         user2 = auth_register('bioman@hotmail.com', 'hello1234', 'Master', 'Splinter')
         with pytest.raises(InputError) as e:
             user_profile_sethandle(user2['token'], 'coolguy')
+
+def test_validToken():
+
+    user1 = auth_register('coolguy@gmail.com', 'hello1234', 'Great', 'Khali')
+    auth_logout[user1['token']]
+    with pytest.raises(AccessError) as e:
+        user_profile_sethandle(user1['token'], 'thebigman')
+
+    user2 = auth_register('goodestman@hotmail.com', 'hello1234', 'Jim', 'Carrey')
+    auth_logout(user2['token'])
+    with pytest.raises(AccessError) as e:
+        user_profile_sethandle(user2['token'], 'funnyman')
+
+    user3 = auth_register('overtime@gmail.com', 'hello1234', 'Light', 'Bulb')
+    auth_logout(user3['token'])
+    with pytest.raises(AccessError) as e:
+        user_profile_sethandle(user3['token'], 'PixarLamp')
