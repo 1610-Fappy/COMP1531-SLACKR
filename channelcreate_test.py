62664574301fd18fb91997b53a9566c29764040c
Jonathan <z5255742@unsw.edu.au>
2020-03-09 14:00:29 +1100

    Fixed branching and made some minor changes to file. Now pushing

--- channelcreate_test.py
+++ channelcreate_test.py
@@ -9,9 +9,9 @@ def test_channel_create():
     User1 = auth_register('User1@gmail.com', 'User1Pass', 'User', '1')
     User2 = auth_register('User2@gmail.com', 'User2Pass', 'User', '2')
     User5 = auth_register('C00lguy@gmail.com', 'MyAwes0mePa55word', 'Brock', 'Lesner')
-    channels_create(User1['Token'], 'First Channel', True)
-    channels_create(User2['Token'], 'Second Channel', False)
-    channels_create(User5['Token'], 'SmackDownVSRaw', True)
+    assert channels_create(User1['Token'], 'First Channel', True)
+    assert channels_create(User2['Token'], 'Second Channel', False)
+    assert channels_create(User5['Token'], 'SmackDownVSRaw', True)
 
 
     User3 = auth_register('User3@gmail.com', 'User3Pass', 'User', '3')
