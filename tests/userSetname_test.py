6571efacc6949a8e8d806df919b626ce4205e682
Abdul Kanj <a.kanj@student.unsw.edu.au>
2020-03-09 13:40:26 +1100

    Added comments and fixed up assert for correct cases

--- tests/userSetname_test.py
+++ tests/userSetname_test.py
@@ -3,28 +3,37 @@ import pytest
 from src.user import user_profile_setname
 from src.auth import auth_register, auth_logout
 
+'''
+ASSUMPTIONS
+* Name cannot have number in it
+'''
+
+
+#testing changing first name correctly with everything valid
 def test_changeFirstName():
 
     user1 = auth_register('knight360@gmail.com', 'hello1234', 'Michael', 'Jordan')
-    user_profile_setname(user1['token'], 'Lebron', 'Jordan')
+    assert(user_profile_setname(user1['token'], 'Lebron', 'Jordan'))
 
     user2 = auth_register('steph@hotmail.com', 'hello1234', 'Stephen', 'Curry')
-    user_profile_setname(user2['token'], 'Lebron', 'Jordan')
+    assert(user_profile_setname(user2['token'], 'Lebron', 'Jordan'))
 
     user3 = auth_register('coolguy@gmail.com', 'hello1234', 'Bomber', 'Man')
-    user_profile_setname(user3['token'], 'Mr', 'Man')
+    assert(user_profile_setname(user3['token'], 'Mr', 'Man'))
 
+#Testing changing last name correctly with everything valid
 def test_changeLastName():
 
     user1 = auth_register('knight360@gmail.com', 'hello1234', 'Michael', 'Jordan')
-    user_profile_setname(user1['token'], 'Michael', 'Lebron')
+    assert(user_profile_setname(user1['token'], 'Michael', 'Lebron'))
 
     user2 = auth_register('steph@hotmail.com', 'hello1234', 'Stephen', 'Curry')
-    user_profile_setname(user2['token'], 'Stephen', 'Lebron')
+    assert(user_profile_setname(user2['token'], 'Stephen', 'Lebron'))
 
     user3 = auth_register('coolguy@gmail.com', 'hello1234', 'Bomber', 'Man')
-    user_profile_setname(user3['token'], 'Bomber', 'Guy')
+    assert(user_profile_setname(user3['token'], 'Bomber', 'Guy'))
 
+#Testing cases for invalid first name
 def test_invalidFirstName():
 
     user1 = auth_register('knight360@gmail.com', 'hello1234', 'Michael', 'Jordan')
@@ -40,6 +49,7 @@ def test_invalidFirstName():
     with pytest.raises(InputError) as e:
         user_profile_setname(user3['token'], 'Bombe23213123123r', 'Man')
 
+#Testing cases for invalid last name
 def test_invalidLastName():
 
     user1 = auth_register('knight360@gmail.com', 'hello1234', 'Michael', 'Jordan')
@@ -54,6 +64,7 @@ def test_invalidLastName():
     with pytest.raises(InputError) as e:
         user_profile_setname(user3['token'], 'Bomber', 'Guy213123')
 
+#Testing for valid token
 def test_validToken():
     user1 = auth_register('knight360@gmail.com', 'hello1234', 'Michael', 'Jordan')
     auth_logout(user1['token'])
