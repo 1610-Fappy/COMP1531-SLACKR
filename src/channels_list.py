f5d57985ea16906c51595d11516dab5ab8fe4620
Jonathan <z5255742@unsw.edu.au>
2020-03-27 14:57:26 +1100

    move all corresponding files into src folder

---
+++ src/channels_list.py
@@ -0,0 +1,13 @@
+""" Channel list function """
+def channels_list(token):
+    """ Implementing channel list function """
+    channel_list = {
+        "token": []
+    }
+
+    if channel_list['token'] == token:
+        return {
+            "channels": {}
+        }
+    return {}
+    
